726fbe5ac225f8dee955d01166a5af45901ea99b
Cesar Pelogia <pelogia.cesar@hotmail.com>
2025-04-18 19:38:13 -0300

    Feat(#36): Adicionado campo de nome ao cadastro de usuários para maior consistência.

--- documentation/database/banco.sql
+++ documentation/database/banco.sql
@@ -50,6 +50,7 @@ CREATE TABLE ImagensApoio (
 
 CREATE TABLE Usuarios (
     id_usuario SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
     senha VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     administrador BOOLEAN NOT NULL DEFAULT FALSE,
